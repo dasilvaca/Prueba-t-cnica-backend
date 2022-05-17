433fac6fe215db8547456a893be1ab1407503664
dasilvaca <dasilvaca@unal.edu.co>
2022-05-17 13:29:36 -0400

    Created some models and began to relate them using foreign keys. Also added admin user to watch each table and model on the app ecosystem

--- ecosystem/admin.py
+++ ecosystem/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Teacher)
+admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Task)
+admin.site.register(CourseMembers)
--- ecosystem/models.py
+++ ecosystem/models.py
@@ -2,21 +2,36 @@ from django.db import models
 
 # Create your models here.
 
-class Courses(models.Model):
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    dni = models.CharField(max_length=10, unique=True, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Course(models.Model):
+    CourseID = models.CharField(max_length=10, unique=True, primary_key=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class Teachers(models.Model):
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    dni = models.CharField(max_length=10, unique=True, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Task(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, default=None)
+    uploaded_task = models.FileField(upload_to='tasks/', default=None)
 
-class Courses(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+class CourseMembers(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
